dcba6d7024fd31c3afcedf15bcbda55fdf94d383
SimoneVidotti <vidottisimone@protonmail.com>
2024-09-24 11:52:39 +0100

    My first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Router minicom:
+<br>
+## Requirements:
+* linux terminal
+* minicom
+* figlet command
+* python3
+<br>
+## Description:
+### This python program is used to automate the execution of minicom command
+### to talk with a router through a USB-Ethernet serial cable.
+<br>
+## How to use:
+- ~# python3 Router_minicom.py
---
+++ Router_minicom.py
@@ -0,0 +1,21 @@
+import os
+
+def a_capo():
+    print('\n')
+
+def Line():
+    print('- - - - - - -')
+
+os.system('figlet -f slant RouterMinicom && sleep 1')
+a_capo()
+
+os.system('lsusb')
+a_capo()
+
+os.system('dmesg | grep tty')
+a_capo()
+
+print('\n! Remember the baud configuration ! (Es: -> 9600 baud)\n')
+
+Porta = input('Choose port name (tree, Es: /dev/ttyUSB0): ')
+Line()
